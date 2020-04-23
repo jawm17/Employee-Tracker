904cc9f6d2e2318a3d7c264e9e892cfceaf725f5
Michael Sanderson <jawm17@gmail.com>
2020-04-23 11:45:09 -0500

    Created schema file, gitignore

---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE employees (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name varchar(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roll_id INT(10) NOT NULL,
+	manager_id INT(10) NOT NULL
+);
+
+CREATE TABLE roles (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(30) NOT NULL,
+    salary DECIMAL(10,2),
+	department_id INT(10) NOT NULL
+);
+
+CREATE TABLE departments (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(30) NOT NULL
+);
